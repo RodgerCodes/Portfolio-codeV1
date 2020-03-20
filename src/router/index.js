01f8d278d9dc8e252a0673b87db0cd7e2a39b0be
RodgerCodes <rkumwanje@gmail.com>
2020-03-20 14:43:50 +0200

    final implementation of the toolbar and footer though on link doesnt work have to fix it later when i will be dealing with it

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Contact from '../views/Contact.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -10,12 +10,10 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
@@ -26,8 +24,9 @@ const routes = [
   {
     path:'/contact',
     name:'Contact',
-    Component: () => import('../views/Contact.vue')
+    Component: Contact
   }
+  
 ];
 
 const router = new VueRouter({
